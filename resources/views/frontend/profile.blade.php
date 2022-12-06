502abd13dcbcee30131dc8ba073bf0ebf6578db2
Min Khant Maw <mr.minkhantmaw@gmail.com>
2022-12-06 22:25:59 +0630

    mobile ui

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -1,5 +1,9 @@
 @extends('frontend.layouts.app')
 @section('title', 'Profile')
 @section('content')
-
+    <div class="account">
+        <div class="profile">
+            <img src="https://ui-avatars.com/api/?background=5842E3&color=fff&name=Min+Khant" class="avatar" alt="">
+        </div>
+    </div>
 @endsection
