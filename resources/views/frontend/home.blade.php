595dd43a75f98440c19496b31e3320c67642b9ac
Min Khant Maw <mr.minkhantmaw@gmail.com>
2022-12-11 20:48:54 +0630

    add ui

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,10 +2,8 @@
 @section('title', 'E-Wallet')
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est temporibus numquam sed beatae doloribus nostrum
-                sapiente quasi vero. Dolores eligendi quibusdam ipsum facere qui culpa dolorem error veritatis tempora
-                accusamus.</p>
+        <div class="d-flex justify-content-center">
+            <h5 class="text-center">Home Page</h5>
         </div>
     </div>
 @endsection
